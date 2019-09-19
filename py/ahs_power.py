d3509cf44ad0c35c9351bddd7842201ede632cd1
jadecodes8191 <jadecodes8191@gmail.com>
2019-09-19 09:29:57 -0400

    sorting stuff

--- py/ahs_power.py
+++ py/ahs_power.py
@@ -1,5 +1,8 @@
 # Copyright 2018 Building Energy Gateway.  All rights reserved.
 
+# New version: Sorts the facilities in ascending order of power usage :)
+# (the old stuff is commented out)
+
 import time
 import pandas as pd
 from building_data_requests import get_value
@@ -12,10 +15,13 @@ start_time = time.time()
 #   - Label
 #   - Facility
 #   - Instance ID of electric meter
-df = pd.read_csv( '../csv/ahs_power.csv' )
+df = pd.read_csv('ahs_power.csv')
+valueLabelsList = [[], [], []]
 
 # Output column headings
-print( 'Feeder,Meter,Units' )
+# print( 'Feeder,Meter,Units' )
+
+maximum = 0
 
 # Iterate over the rows of the dataframe, getting meter readings for each feeder
 for index, row in df.iterrows():
@@ -26,8 +32,31 @@ for index, row in df.iterrows():
     value = int( value ) if isinstance( value, numbers.Number ) else ''
     units = units if units else ''
 
+    # if value > maximum:
+    #   maximum = value
+    valueLabelsList[0].append(value)
+    valueLabelsList[1].append(row['Label'])
+    valueLabelsList[2].append(value)
+
+
+#df = df.sort_values(by='Label', ascending=False)
+valueLabelsList[0].sort()
+# print(valueLabelsList[0])
+# print(valueLabelsList[1])
+# print(valueLabelsList[2])
+
+print("Sorted: ")
+
+for val in valueLabelsList[0]:
+    tempIdx = valueLabelsList[2].index(val)
+    temp = str(valueLabelsList[1][tempIdx])
+    print(temp)
+    print(val)
+
+
+# for index, row in df.iterrows():
     # Output CSV format
-    print( '{0},{1},{2}'.format( row['Label'], value, units ) )
+    # print( '{0},{1},{2}'.format( row['Label'], value, units ) )
 
 # Report elapsed time
 elapsed_time = round( ( time.time() - start_time ) * 1000 ) / 1000
