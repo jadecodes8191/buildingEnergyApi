5cd16b5ee8c196ecfdf82e34eaa6cbf58741ebdd
jadecodes8191 <jadecodes8191@gmail.com>
2019-12-18 08:21:48 -0500

    updating clear methods -- to be accessed easily during testing

--- clear_daily.py
+++ clear_daily.py
@@ -2,7 +2,6 @@
 should be run once a week: clear out daily database and others, or maybe save them to a more permanent database, just to
 clear for the next weekly report.
 """
-import pandas as pd
 import sqlite3
 
 PATH = 'my_file'
@@ -10,12 +9,10 @@ PATH = 'my_file'
 conn = sqlite3.connect(PATH)
 cursor = conn.cursor()
 
-drop = "DROP TABLE ColdDatabase"
-drop2 = "DROP TABLE WarmDatabase"
-drop3 = "DROP TABLE CarbonDatabase"
+drop = "DROP TABLE TemperatureProblemsDatabase"
+drop2 = "DROP TABLE CarbonDioxideProblemsDatabase"
 
 cursor.execute(drop)
 cursor.execute(drop2)
-cursor.execute(drop3)
 
 conn.close()
--- clear_weekly.py
+++ clear_weekly.py
@@ -2,7 +2,6 @@
 should be run once a week: clear out daily database and others, or maybe save them to a more permanent database, just to
 clear for the next weekly report.
 """
-import pandas as pd
 import sqlite3
 
 PATH = 'my_file'
@@ -11,7 +10,15 @@ conn = sqlite3.connect(PATH)
 cursor = conn.cursor()
 
 drop = "DROP TABLE DailyDatabase"
+drop2 = "DROP TABLE DailyTempDatabase"
+drop3 = "DROP TABLE DailyCarbonDatabase"
 
 cursor.execute(drop)
+cursor.execute(drop2)
+cursor.execute(drop3)
 
 conn.close()
+
+
+
+
