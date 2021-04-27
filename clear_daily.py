b6df14008a7d43126b41ffcb983a7d1ab063e6ae
jadecodes8191 <jadecodes8191@gmail.com>
2021-04-27 10:40:58 -0400

    finalizing full metasys script

--- clear_daily.py
+++ clear_daily.py
@@ -9,10 +9,10 @@ PATH = 'my_file'
 conn = sqlite3.connect(PATH)
 cursor = conn.cursor()
 
-drop = "DROP TABLE TemperatureProblemsDatabase"
-drop2 = "DROP TABLE CarbonDioxideProblemsDatabase"
+# Still doesn't work
+# drop = "DELETE FROM TempAndCO2Log WHERE CO2='2020 Sept - AHS ZN-T OA-T & CO2.csv'"
+drop = "DELETE FROM MetasysLog WHERE 1=1"
 
 cursor.execute(drop)
-cursor.execute(drop2)
 
 conn.close()
