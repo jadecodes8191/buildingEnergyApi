93439f4d8fe18e604dc975497720635ec1d83797
jadecodes8191 <jadecodes8191@gmail.com>
2021-04-28 14:54:50 -0400

    Revert "finalizing full metasys script"
    This reverts commit b6df14008a7d43126b41ffcb983a7d1ab063e6ae.

--- clear_daily.py
+++ clear_daily.py
@@ -9,6 +9,7 @@ PATH = 'MetasysWeeklyDatabase'
 conn = sqlite3.connect(PATH)
 cursor = conn.cursor()
 
+<<<<<<< HEAD
 # Still doesn't work
 # drop = "DELETE FROM TempAndCO2Log WHERE CO2='2020 Sept - AHS ZN-T OA-T & CO2.csv'"
 # drop = "DELETE FROM MetasysLog WHERE 1=1"
@@ -19,5 +20,12 @@ cursor.execute(drop)
 drop = "DROP TABLE MetasysHighCO2Values"
 cursor.execute(drop)
 
+=======
+drop = "DROP TABLE TemperatureProblemsDatabase"
+drop2 = "DROP TABLE CarbonDioxideProblemsDatabase"
+
+cursor.execute(drop)
+cursor.execute(drop2)
+>>>>>>> parent of b6df140... finalizing full metasys script
 
 conn.close()
